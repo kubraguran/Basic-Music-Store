1f33d2556fbf2591c63668eea65694838994cbdc
kubraguran <81765398+kubraguran@users.noreply.github.com>
2021-11-21 13:25:08 +0300

    Add files via upload

---
+++ music/denemeler.js
@@ -0,0 +1,47 @@
+
+//document.getElementById("mainCont").style.background = "white";
+
+/* function changeColor () {
+    let color = document.getElementById("color");
+    color.addEventListener("click" , toggle);
+};
+
+function toggle() {
+    let changes = document.getElementById("mainCont");
+    let backColor = color.style.backgroundColor;
+    changes.style.backgroundColor = backColor === "black" ? "white" : black;
+};
+
+changeColor();
+*/
+
+
+//SAMPLE
+/* 
+
+function start() {
+  var submit = document.getElementById("submit");
+  submit.addEventListener("click", toggle);
+};
+
+function toggle() {
+  var color = document.getElementById("body");
+  var backColor = color.style.backgroundColor;
+  color.style.backgroundColor = backColor === "black" ? "white" : "black";
+};
+
+
+start();
+
+
+-------
+<body id="body">
+   <button id="submit">Submit
+      <label id="switch">
+          <input type="checkbox" checked>
+          <span class="slider"></span>
+      </label>
+   </button>
+</body>
+
+*/
